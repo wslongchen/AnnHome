f196d5b2d68e2245a026e0023622d8530dec44d4
潘安 <wslongchen@vip.qq.com>
2015-12-02 11:47:22 +0800

    为recylecview增加点击事件
    。。。

--- Android客户端/AnnHome/app/src/main/java/com/example/mrpan/annhome/MainActivity.java
+++ Android客户端/AnnHome/app/src/main/java/com/example/mrpan/annhome/MainActivity.java
@@ -1,12 +1,21 @@
 package com.example.mrpan.annhome;
 
+import android.animation.Animator;
 import android.app.Activity;
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.transition.ChangeTransform;
+import android.transition.Transition;
+import android.util.Log;
+import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
@@ -23,27 +32,29 @@ public class MainActivity extends Activity {
 
     private SlidingMenu slidingMenu = null;
 
-    public boolean log_State;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    //初始化操作
+    private void init(){
         // 设置抽屉菜单
         slidingMenu = new SlidingMenu(this);
         slidingMenu.setMode(SlidingMenu.LEFT_RIGHT);
         slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_MARGIN); // 触摸边界拖出菜单
         slidingMenu.setMenu(R.layout.slidingmenu_left);
-        if (log_State) {
-            slidingMenu.setSecondaryMenu(R.layout.slidingmenu_right_havelogin);
-        } else {
-            slidingMenu.setSecondaryMenu(R.layout.slidingmenu_right);
-        }
+        slidingMenu.setSecondaryMenu(R.layout.slidingmenu_right);
+//        if (log_State) {
+//            slidingMenu.setSecondaryMenu(R.layout.slidingmenu_right_havelogin);
+//        } else {
+//            slidingMenu.setSecondaryMenu(R.layout.slidingmenu_right);
+//        }
         slidingMenu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
-
         // 将抽屉菜单与主页面关联起来
         slidingMenu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
-        //Gson gson=new Gson();
         // 拿到RecyclerView
         mRecyclerView = (RecyclerView) findViewById(R.id.list);
         // 设置LinearLayoutManager
@@ -57,19 +68,45 @@ public class MainActivity extends Activity {
         // 为mRcyclerView设置适配器
         mRecyclerView.setAdapter(myAdapter);
 
-
-
+        this.myAdapter.setOnItemClickListener(new MyAdapter.MyItemClickListener() {
+            @Override
+            public void onItemClick(View view, int postion) {
+                Toast.makeText(MainActivity.this, "11111111111111", Toast.LENGTH_SHORT).show();
+                startActivity(view);
+            }
+        });
+        this.myAdapter.setOnItemLongClickListener(new MyAdapter.MyItemLongClickListener() {
+            @Override
+            public void onItemLongClick(View view, int postion) {
+                Toast.makeText(MainActivity.this, "22222222222222", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
+    //设置数据
     private ArrayList<HashMap<String,Object>> getItems(){
         ArrayList<HashMap<String,Object>> items=new ArrayList<HashMap<String, Object>>();
         for(int i=0;i<5;i++) {
             HashMap<String,Object> item=new HashMap<String,Object>();
             item.put("title", "上海");
             items.add(item);
-
         }
 
         return items;
     }
+
+    //跳转事件
+    public void startActivity(final View v) {
+        View pic = v.findViewById(R.id.pic);
+        Transition ts = new ChangeTransform();
+        ts.setDuration(3000);
+        getWindow().setExitTransition(ts);
+        Bundle bundle = ActivityOptions.makeSceneTransitionAnimation((Activity) this,
+                Pair.create(pic, 1 + "pic"),
+                Pair.create(pic, 1 + "pic")).toBundle();
+        Intent intent = new Intent(this, DetailActivity.class);
+        intent.putExtra("pos", 1);
+        startActivity(intent);
+    }
+
 }
--- Android客户端/AnnHome/app/src/main/java/com/example/mrpan/annhome/MyAdapter.java
+++ Android客户端/AnnHome/app/src/main/java/com/example/mrpan/annhome/MyAdapter.java
@@ -11,15 +11,18 @@ import android.widget.TextView;
 
 import java.util.HashMap;
 import java.util.List;
-import java.util.Objects;
 
 /**
  * Created by mrpan on 15/11/18.
  */
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
 
+
     private List<HashMap<String,Object>> items;
     private Context mContext;
+
+    private MyItemClickListener mItemClickListener;
+    private MyItemLongClickListener mItemLongClickListener;
 
     public MyAdapter(Context context,List<HashMap<String,Object>> items){
         this.mContext=context;
@@ -29,9 +32,24 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
     @Override
     public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         //设置布局文件
-        View v= LayoutInflater.from(parent.getContext()).inflate(R.layout.articles,parent,false);
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.articles, parent,false);
+        ViewHolder vh = new ViewHolder(itemView,mItemClickListener,mItemLongClickListener);
+        return vh;
+    }
 
-        return new MyAdapter.ViewHolder(v);
+    /**
+     * 设置Item点击监听
+     * @param listener
+     */
+    public void setOnItemClickListener(MyItemClickListener listener){
+        this.mItemClickListener = listener;
+    }
+
+    /**
+     * 设置Item长击监听
+     */
+    public void setOnItemLongClickListener(MyItemLongClickListener listener){
+        this.mItemLongClickListener = listener;
     }
 
     @Override
@@ -49,6 +67,9 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
         return items == null ? 0 : items.size();
     }
 
+    /*
+    * 设置添加的数据项
+    * */
     public void addItems(List<HashMap<String,Object>> itemsAdd){
         //添加数据项
         this.items.addAll(itemsAdd);
@@ -57,20 +78,57 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
     // 重写的自定义ViewHolder
     public static class ViewHolder
             extends RecyclerView.ViewHolder
+            implements View.OnClickListener,View.OnLongClickListener
     {
+        //控件
         public TextView title;
-
         public ImageView mImageView;
-
         public RelativeLayout re;
 
-        public ViewHolder( View v )
-        {
+        //上下文
+        public Context mContext;
+
+        //监听
+        private MyItemClickListener mListener;
+        private MyItemLongClickListener mLongClickListener;
+
+        public ViewHolder( View v ,MyItemClickListener listener,MyItemLongClickListener longClickListener) {
             super(v);
+            //绑定并设置监听事件
             title = (TextView) v.findViewById(R.id.title);
             mImageView = (ImageView) v.findViewById(R.id.pic);
             re=(RelativeLayout)v.findViewById(R.id.picture);
+            this.mListener = listener;
+            this.mLongClickListener = longClickListener;
+            v.setOnClickListener(this);
+            v.setOnLongClickListener(this);
+
         }
+
+        @Override
+        public void onClick(View v) {
+            if(mListener != null){
+                mListener.onItemClick(v,getPosition());
+            }
+        }
+
+        @Override
+        public boolean onLongClick(View v) {
+            if(mLongClickListener != null){
+                mLongClickListener.onItemLongClick(v, getPosition());
+            }
+            return true;
+        }
+    }
+
+    /*
+    * 仿Listview声明Item监听接口*/
+    public interface MyItemClickListener {
+        public void onItemClick(View view,int postion);
+    }
+
+    public interface MyItemLongClickListener {
+        public void onItemLongClick(View view,int postion);
     }
 
 }
