3c0b6ef91cd9d846229d09d02aea510b1f5b934d
潘安 <wslongchen@vip.qq.com>
2016-01-06 17:26:19 +0800

    顶部菜单修改
    。。。

--- Web客户端/AnnHome/header.php
+++ Web客户端/AnnHome/header.php
@@ -58,21 +58,29 @@
 							if (is_home()) { echo 'class="active"';
 							}
  ?>><a href="<?php echo get_option('home'); ?>/">首页</a></li>
-							<?php wp_list_categories('depth=1&title_li=0&sort_column=menu_order'); ?>
-								<!--<?php
-$args=array(
-'orderby' => 'name',
-'order' => 'ASC',
-'child_of'=>10
-);
-$categories=get_categories($args);
-echo ' <li class="dropdown" role="presentation">';
-foreach($categories as $category) {
-echo ' <lu class="dropdown-menu">';
-echo ' <a href="' . get_category_link( $category->term_id ) . '" title="' . $category->name . '" ' . '>' . $category->name.' </a>';
-echo ' </li>';
-}
-?>-->
+							<!--<?php wp_list_categories('depth=1&title_li=0&sort_column=menu_order'); ?>-->
+								<?php
+								$args = array('orderby' => 'name', 'parent' => 0);
+								$categories = get_categories($args);
+								foreach ($categories as $category) {
+									if (get_category_children($category->term_id) != "") {
+										$args2 = array('orderby' => 'name', 'child_of' => $category->term_id);
+										$categories2 = get_categories($args2);
+											echo '<li class="dropdown">
+												<a href="'.get_category_link($category -> term_id).'" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">'.$category->name.'<span class="badge">'.$category->count.'</span><span class="caret"></span></a>';
+											echo '<ul class="dropdown-menu">';
+										foreach ($categories2 as $category2) {
+											echo '<li><a href="'.get_category_link($category2 -> term_id).'" id="tencent">'.$category2->name.'<span class="badge">'.$category2->count.'</span></a></li>';
+										}
+											echo '</ul></li>';
+									} else {
+										echo ' <li role="presentation">';
+										echo ' <a href="' . get_category_link($category -> term_id) . '">' . $category -> name . '<span class="badge">'.$category->count.'</span> </a>';
+										echo ' </li>';
+									}
+									
+								}
+							?>
 							<?php wp_list_pages('depth=1&title_li=0&sort_column=menu_order'); ?>
 						</ul>
 					</ul>
@@ -88,7 +96,7 @@ echo ' </li>';
 									<a href="javascript:void(function(){var d=document,e=encodeURIComponent,s1=window.getSelection,s2=d.getSelection,s3=d.selection,s=s1?s1():s2?s2():s3?s3.createRange().text:'',r='http://www.douban.com/recommend/?url='+e(d.location.href)+'&title='+e(d.title)+'&sel='+e(s)+'&v=1',w=450,h=330,x=function(){if(!window.open(r,'douban','toolbar=0,resizable=1,scrollbars=yes,status=1,width='+w+',height='+h+',left='+(screen.width-w)/2+',top='+(screen.height-h)/2))location.href=r+'&r=1'};if(/Firefox/.test(navigator.userAgent)){setTimeout(x,0)}else{x()}})()"><img src="http://img3.douban.com/pics/fw2douban1.png" alt="推荐到豆瓣" /></a>
 								</li>
 								<li role="separator" class="divider"></li>
-								<li><a href="#" id="sina">Github</a></li>
+								<li><a href="#" id="sina">新浪微博</a></li>
 							</ul>
 						</li>
 					</ul>
